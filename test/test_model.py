cebaa2c59f16f1396d2a188d37cf06c4c8aba925
fimbo4 <nihu21@student.bth.se>
2023-05-22 10:53:12 +0200

    delade upp några tester

--- test/test_model.py
+++ test/test_model.py
@@ -12,22 +12,34 @@ class ModelTest(unittest.TestCase):
 
     cn = CommunicationNetwork({'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']}, {'h1': 1, 'h2': 2, 'h3': 3})
 
-    def test_vertices(self):
+    def test_vertices_len(self):
         self.assertEqual(len(ModelTest.cn.vertices()), 4)
+
+    def test_vertices(self):
         self.assertEqual(ModelTest.cn.vertices('h1'), {'v1', 'v2'})
 
-    def test_hyperedges(self):
+    def test_hyperedges_len(self):
         self.assertEqual(len(ModelTest.cn.hyperedges()), 3)
+
+    def test_hyperedges(self):
         self.assertEqual(ModelTest.cn.hyperedges('v1'), {'h1'})
 
 
 class ModelDataTest(unittest.TestCase):
-    def test_model_with_data(self):
+    def test_model_with_data_participants(self):
         communciation_network = CommunicationNetwork.from_json('./data/networks/microsoft.json.bz2')
         self.assertEqual(len(communciation_network.participants()), 37103)
-        self.assertEqual(len(communciation_network.channels()), 309740)
 
+    def test_model_with_data_channels(self):
+            communciation_network = CommunicationNetwork.from_json('./data/networks/microsoft.json.bz2')
+            self.assertEqual(len(communciation_network.channels()), 309740)
+
+    def test_model_with_data_vertices(self):
+        communciation_network = CommunicationNetwork.from_json('./data/networks/microsoft.json.bz2')
         self.assertEqual(len(communciation_network.vertices()), 37103)
+    
+    def test_model_with_data_hyperedges(self):
+        communciation_network = CommunicationNetwork.from_json('./data/networks/microsoft.json.bz2')
         self.assertEqual(len(communciation_network.hyperedges()), 309740)
 
 
@@ -57,14 +69,26 @@ class TestTimeVaryingHypergraph(unittest.TestCase):
         self.assertEqual({"h1": 1, "h2":32}, hypergraph.timings())
 
 
-    def test_TimeVaryingHyperGraph_empty(self):
+    def test_TimeVaryingHyperGraph_empty_hyperedges(self):
         """Tests the hyperedges, timings and vertices functions with empty input"""
         
         hypergraph = TimeVaryingHypergraph({}, {})
 
         self.assertEqual({}, hypergraph.hyperedges())
-        self.assertEqual({}, hypergraph.timings())
-        self.assertEqual({}, hypergraph.vertices())
+    
+        def test_TimeVaryingHyperGraph_empty_timings(self):
+            """Tests the hyperedges, timings and vertices functions with empty input"""
+            
+            hypergraph = TimeVaryingHypergraph({}, {})
+
+            self.assertEqual({}, hypergraph.timings())
+
+        def test_TimeVaryingHyperGraph_empty_vertices(self):
+            """Tests the hyperedges, timings and vertices functions with empty input"""
+            
+            hypergraph = TimeVaryingHypergraph({}, {})
+
+            self.assertEqual({}, hypergraph.vertices())
 
     def test_TimeVaryingHyperGraph_no_vertices(self):
         """Tests the vertices class function with no vertices"""
@@ -73,7 +97,14 @@ class TestTimeVaryingHypergraph(unittest.TestCase):
 
         #gives set() when empty not {}
         self.assertEqual({}, hypergraph.vertices())
-        self.assertEqual(len(hypergraph.vertices()), 0)
+
+        def test_TimeVaryingHyperGraph_no_vertices_len(self):
+            """Tests the vertices class function with no vertices"""
+            
+            hypergraph = TimeVaryingHypergraph({"h1":[], "h2":[]}, {"h1": 1, "h2": 2})
+
+            #gives set() when empty not {}
+            self.assertEqual(len(hypergraph.vertices()), 0)
 
 
     def test_TimeVaryingHyperGraph_unknown_edge(self):
@@ -93,14 +124,26 @@ class TestTimeVaryingHypergraph(unittest.TestCase):
     
 
     def test_time_varying_hypergraph_exceptions_input_strings(self):
-        hyper_graph = TimeVaryingHypergraph({'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']}, {'h1': 'words', 'h2': 'words', 'h3': 'words'})#changed integers to strings
+        """testing how the datastructure handles inputs that are all strings"""
+        hyper_graph = TimeVaryingHypergraph({'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']}, {'h1': 'words', 'h2': 'words', 'h3': 'words'})
         self.assertRaises(hyper_graph)
 
     def test_time_varying_hypergraph_exceptions_input_ints(self):
-        hyper_graph = TimeVaryingHypergraph({1: [1, 2], 2: [2, 3], 3: [3, 4]}, {1: 1, 2: 2, 3: 3})#changed strings to integers
+        """testing how the datastructure handles inputs that are all integers"""
+        hyper_graph = TimeVaryingHypergraph({1: [1, 2], 2: [2, 3], 3: [3, 4]}, {1: 1, 2: 2, 3: 3})
         self.assertRaises(hyper_graph)
 
-    def test_datastructure_constructor(self):
+    def test_datastructure_constructor_hedges(self):
+        """this unittest is intented to make sure that the constructor works properly for the hyperedges"""
+        hyper_graph = TimeVaryingHypergraph({'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']}, {'h1': 1, 'h2': 2, 'h3': 3})
+        self.assertEqual(hyper_graph._hedges, {'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']})
+
+    def test_datastructure_constructor_vertices(self):
+        """this unittest is intented to make sure that the constructor works properly for the vertices"""
+        hyper_graph = TimeVaryingHypergraph({'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']}, {'h1': 1, 'h2': 2, 'h3': 3})
+        self.assertEqual(hyper_graph._vertices, {'v1': ['h1'], 'v2': ['h1', 'h2'], 'v3': ['h2', 'h3'], 'v4': ['h3']})
+    
+    def test_hypergraph_timings(self):
         pass
             
             
